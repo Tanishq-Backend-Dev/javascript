69f71d07329b1cf8860f95a1af0c452c6cacafd9
Tanishq-Backend-Dev <154458664+Tanishq-Backend-Dev@users.noreply.github.com>
2024-02-23 21:25:34 +0000

    commit 5 videos

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version.
+
+let name = "Tanishq"; //String
+let age = 23;         //Number
+let isLoggedIn = true; //Boolean
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => varaible is declared but not initialised any value
+// symbol => unique value
+
+
+// object => collection of data
+
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undefined
+
+
