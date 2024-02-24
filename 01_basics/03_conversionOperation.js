d2d439a5a9a985a4d44bef4abf9920e6b9e96186
Tanishq-Backend-Dev <154458664+Tanishq-Backend-Dev@users.noreply.github.com>
2024-02-24 16:14:07 +0000

    Total  7 Videos

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "33";
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Tanishq" => true
+
+// ******************Operations****************** //
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Tanishq";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);  // 12
+// console.log(1 + "2");  // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
