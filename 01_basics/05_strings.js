f8f8dbc49c59ec5a7c44471a2b9c4f748f382652
Tanishq-Backend-Dev <154458664+Tanishq-Backend-Dev@users.noreply.github.com>
2024-02-25 16:27:11 +0000

    commit string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "Tanishq";
+const repoCount = 50;
+// console.log(name + repoCount);
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Tanishq-Thakur');
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('i'));
+
+// const newString = gameName.substring(1,6);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-3,4);
+// console.log(anotherString);
+
+// const newStringOne = "  tanishq   ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary";
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('hitesh'));
+
+// console.log(gameName.split('-'));
+
+// let str1 = "Tanishq";
+// let str2 = "Thakur";
+// let concatStr = str1.concat(" ",str2);
+// console.log(concatStr);
+
+let str = "Microsoft is a very good company";
+console.log(str.replace('Microsoft','Amazon'));
+console.log(str.split(' '));
